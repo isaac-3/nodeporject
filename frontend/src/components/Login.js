a79ca2b98b37bbcfbc7e59849370093dc935d0e9
isaac-3 <isaacgc0596@gmail.com>
2020-09-04 23:08:33 -0500

    frontend routes

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div className="mycard">
+            <div className="card auth-card input-field">
+                <h2>Instagram</h2>
+                <input
+                type="text"
+                placeholder="email"
+                />
+                <input
+                type="text"
+                placeholder="password"
+                />
+                <button className="btn waves-effect waves-light #64b5f6 blue lighten-2">Login
+                </button>
+            </div>
+        </div>
+    );
+}
+ 
+export default Login;
