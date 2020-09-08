446c712d7ac9e93455233ee74c2af32b9db725e9
isaac-3 <isaacgc0596@gmail.com>
2020-09-07 22:03:04 -0500

    fcreate post,like/unlike,comments

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -4,9 +4,10 @@ const mongoose = require('mongoose')
 const requireLogin = require('../middleware/requireLogin')
 const Post = mongoose.model('Post')
 
-router.get('/allpost', (req, res) => {
+router.get('/allpost', requireLogin, (req, res) => {
     Post.find()
     .populate("postedBy", "_id name")
+    .populate("comments.postedBy", "_id name")
     .then(posts=> {
         res.json({posts})
     })
@@ -24,7 +25,7 @@ router.post('/createpost', requireLogin, (req, res) => {
     const post = new Post({
         title,
         body,
-        pic,
+        photo: pic,
         postedBy: req.user
     })
     post.save().then(result => {
@@ -43,6 +44,55 @@ router.get('/mypost', requireLogin, (req, res) => {
     })
     .catch(err => {
         console.log(err)
+    })
+})
+
+router.put('/like', requireLogin, (req, res) => {
+    Post.findByIdAndUpdate(req.body.postId, {
+        $push:{likes: req.user._id}
+    },{
+        new: true
+    }).exec((err, result)=>{
+        if(err){
+            return res.status(422).json({error: err})
+        }else{
+            res.json(result)
+        }
+    })
+})
+
+router.put('/unlike', requireLogin, (req, res) => {
+    Post.findByIdAndUpdate(req.body.postId, {
+        $pull:{likes: req.user._id}
+    },{
+        new: true
+    }).exec((err, result)=>{
+        if(err){
+            return res.status(422).json({error: err})
+        }else{
+            res.json(result)
+        }
+    })
+})
+
+router.put('/comment', requireLogin, (req, res) => {
+    const comment = {
+        text: req.body.text,
+        postedBy: req.user._id
+    }
+    Post.findByIdAndUpdate(req.body.postId, {
+        $push:{comments: comment}
+    },{
+        new: true
+    })
+    .populate("postedBy", "_id name")
+    .populate("comments.postedBy", "_id name")
+    .exec((err, result)=>{
+        if(err){
+            return res.status(422).json({error: err})
+        }else{
+            res.json(result)
+        }
     })
 })
 
--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React,{useEffect, useState, useContext} from 'react';
+import {UserContext} from '../App'
+
 
 const Profile = () => {
+
+    const [posts, setPosts] = useState([])
+    const {state, dispatch} = useContext(UserContext)
+
+    useEffect(()=>{
+        fetch("http://localhost:3001/mypost", {
+            headers: {"Authorization": "Bearer "+localStorage.getItem("jwt")}
+        })
+        .then(res => res.json())
+        .then(res => {
+            setPosts(res.mypost)
+        })
+    },[]) 
+    
     return (
         <div style={{maxWidth: "550px", margin: '0px auto'}}>
             <div style={{display: "flex", justifyContent: "space-around", margin: "80px 0px", borderBottom:"1px solid grey"}}>
@@ -10,7 +26,7 @@ const Profile = () => {
                     />
                 </div>
                 <div>
-                    <h4>isaac chavez</h4>
+                    <h4>{state ? state.name : ". . ."}</h4>
                     <div style={{display: "flex", justifyContent: "space-between", width:"108%"}}>
                         <h6>40 post</h6>
                         <h6>40 followers</h6>
@@ -20,14 +36,13 @@ const Profile = () => {
             </div>
 
             <div className="gallery">
-                <img className="item" src="https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"/>
-                <img className="item" src="https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"/>
-                <img className="item" src="https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"/>
-                <img className="item" src="https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"/>
-                <img className="item" src="https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"/>
-                <img className="item" src="https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"/>
-                <img className="item" src="https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"/>
-                <img className="item" src="https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"/>
+                {
+                    posts.map(post=>{
+                        return(
+                            <img className="item" src={post.photo} key={post._id} alt={post.title}/>
+                        )
+                    })
+                }
             </div>
         </div>
     );
