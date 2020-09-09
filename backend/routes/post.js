3197806a435e5f68e50f121f46fac782959dbe4b
isaac-3 <isaacgc0596@gmail.com>
2020-09-08 23:59:55 -0500

    deleteing routes

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -52,7 +52,10 @@ router.put('/like', requireLogin, (req, res) => {
         $push:{likes: req.user._id}
     },{
         new: true
-    }).exec((err, result)=>{
+    })
+    .populate("comments.postedBy", "_id name")
+    .populate("postedBy", "_id name")
+    .exec((err, result)=>{
         if(err){
             return res.status(422).json({error: err})
         }else{
@@ -66,7 +69,10 @@ router.put('/unlike', requireLogin, (req, res) => {
         $pull:{likes: req.user._id}
     },{
         new: true
-    }).exec((err, result)=>{
+    })
+    .populate("comments.postedBy", "_id name")
+    .populate("postedBy", "_id name")
+    .exec((err, result)=>{
         if(err){
             return res.status(422).json({error: err})
         }else{
@@ -85,8 +91,8 @@ router.put('/comment', requireLogin, (req, res) => {
     },{
         new: true
     })
-    .populate("postedBy", "_id name")
     .populate("comments.postedBy", "_id name")
+    .populate("postedBy", "_id name")
     .exec((err, result)=>{
         if(err){
             return res.status(422).json({error: err})
@@ -95,5 +101,39 @@ router.put('/comment', requireLogin, (req, res) => {
         }
     })
 })
+
+router.delete('/deletepost/:postId', requireLogin, (req, res) => {
+    Post.findOne({_id: req.params.postId})
+    .populate("postedBy", "_id")
+    .exec((err, post)=>{
+        if(err || !post){
+            return res.status(422).json({error: err})
+        }
+        if(post.postedBy._id.toString() === req.user._id.toString()){
+            post.remove()
+            .then(result => {
+                res.json(result)
+            }).catch(err => console.log(err))
+        }
+    })
+})
+
+router.delete('/deletecomment/:postId', requireLogin, (req, res) => {
+    Post.findByIdAndUpdate(req.params.postId, {
+        $pull:{comments: {_id: req.body.commentId}}
+    },{
+        new: true
+    })
+    .populate("comments.postedBy", "_id name")
+    .populate("postedBy", "_id name")
+    .exec((err, result)=>{
+        if(err){
+            return res.status(422).json({error: err})
+        }else{
+            res.json(result)
+        }
+    })
+})
+
 
 module.exports = router
