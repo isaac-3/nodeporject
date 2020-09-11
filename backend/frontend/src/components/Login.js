5b9f4688a4dfb23aa7a18a8bfd4029fb9e656afb
isaac-3 <isaacgc0596@gmail.com>
2020-09-11 16:19:19 -0500

    ready

--- backend/frontend/src/components/Login.js
+++ backend/frontend/src/components/Login.js
@@ -56,7 +56,7 @@ const Login = () => {
                 <button className="btn waves-effect waves-light #64b5f6 blue darken-1"
                     onClick={()=>postdata()}
                 >Login</button>
-                <h5><Link to='/signup'>Dont have an account ?</Link></h5>
+                <h5><Link to='/signup'>Dont have an account ? Sign Up!</Link></h5>
             </div>
         </div>
     );
